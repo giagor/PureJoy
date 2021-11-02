2db4bb4d3a4ac27df156b353e566c304807fe08d
18822835767 <1165407654@qq.com>
2021-11-02 23:47:19 +0800

    feat(home): Create "home" module

---
+++ home/src/main/java/com/topview/purejoy/home/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.topview.purejoy.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity_home)
+    }
+}
