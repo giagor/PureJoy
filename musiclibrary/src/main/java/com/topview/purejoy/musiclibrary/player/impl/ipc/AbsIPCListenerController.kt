e176df82f599f57d8b26e741c5cdf00f7b6c8301
lanlin-code <2529813756@qq.com>
2021-11-04 16:24:57 +0800

    feat(music): add specific implementation of callback management

---
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/player/impl/ipc/AbsIPCListenerController.kt
@@ -0,0 +1,30 @@
+package com.topview.purejoy.musiclibrary.player.impl.ipc
+
+import com.topview.purejoy.musiclibrary.IPCListenerController
+import com.topview.purejoy.musiclibrary.data.Wrapper
+
+abstract class AbsIPCListenerController : IPCListenerController.Stub() {
+    /**
+     * @param change 数据集合中变化的数据
+     * 通知客户端数据集合的变化
+     */
+    abstract fun invokeDataSetChangeListener(change: List<Wrapper>?)
+
+    /**
+     * @param change 当前准备播放的音乐实体
+     * 通知客户端播放的音乐实体发生变化
+     */
+    abstract fun invokeItemChangeListener(change: Wrapper)
+
+    /**
+     * @param mode 播放模式
+     * 通知客户端播放模式发生变化
+     */
+    abstract fun invokeModeChangeListener(mode: Int)
+
+    /**
+     * @param state 播放状态
+     * 通知客户端音乐播放器的播放状态发生变化
+     */
+    abstract fun invokePlayStateChangeListener(state: Boolean)
+}
