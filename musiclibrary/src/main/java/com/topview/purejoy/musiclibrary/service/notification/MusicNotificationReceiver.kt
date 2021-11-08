5f915771e1a14b3d6f8182f9ca8a0ada0a065d54
lanlin-code <2529813756@qq.com>
2021-11-08 20:39:10 +0800

    feat(music): front desk notification for music playback

---
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/service/notification/MusicNotificationReceiver.kt
@@ -0,0 +1,36 @@
+package com.topview.purejoy.musiclibrary.service.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.topview.purejoy.musiclibrary.IPCPlayerController
+
+class MusicNotificationReceiver(
+    private val controller: IPCPlayerController,
+    var listener: ClearListener? = null) : BroadcastReceiver() {
+
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action) {
+            PREVIOUS_ACTION -> controller.last()
+            NEXT_ACTION -> controller.next()
+            STATE_ACTION -> controller.playOrPause()
+            CLEAR_ACTION -> listener?.onClear()
+            else -> {
+
+            }
+        }
+    }
+
+    interface ClearListener {
+        fun onClear()
+    }
+
+    companion object {
+        const val PREVIOUS_ACTION = "com.topview.purejoy.musiclibrary.previous"
+        const val NEXT_ACTION = "com.topview.purejoy.musiclibrary.next"
+        const val STATE_ACTION = "com.topview.purejoy.musiclibrary.state"
+        const val CLEAR_ACTION = "com.topview.purejoy.musiclibrary.clear"
+        const val TAG = "NotificationReceiver"
+    }
+}
