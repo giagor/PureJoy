b5da9082641d614941f48a7762c65c17938c5a6a
lanlin-code <2529813756@qq.com>
2021-11-13 16:54:10 +0800

    feat(music): add the function of getting picture color

---
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/common/util/Util.kt
@@ -0,0 +1,45 @@
+package com.topview.purejoy.musiclibrary.common.util
+
+import android.graphics.Bitmap
+import androidx.palette.graphics.Palette
+import com.topview.purejoy.musiclibrary.R
+
+/**
+ * 获取图片主题色的方法
+ * @param bitmap 从这个Bitmap获取主题色
+ * @param view 要设置背景色的View
+ */
+fun buildSwatch(bitmap: Bitmap, action: (Int) -> Unit) {
+    Palette.Builder(bitmap).generate {
+        it?.apply {
+            val s = when {
+                lightMutedSwatch != null -> {
+                    lightMutedSwatch
+                }
+                lightVibrantSwatch != null -> {
+                    lightVibrantSwatch
+                }
+                vibrantSwatch != null -> {
+                    vibrantSwatch
+                }
+                vibrantSwatch != null -> {
+                    vibrantSwatch
+                }
+                darkMutedSwatch != null -> {
+                    darkMutedSwatch
+                }
+                darkVibrantSwatch != null -> {
+                    darkVibrantSwatch
+                }
+                else -> {
+                    mutedSwatch
+                }
+            }
+            val c = s?.rgb ?: R.color.design_default_color_primary
+//                    view.setBackgroundColor(c)
+            action.invoke(c)
+
+        }
+    }
+
+}
