131b4bf289c9c7460a33b4da9508023a79ccdc1c
18822835767 <1165407654@qq.com>
2021-11-13 19:38:45 +0800

    feat(home): Add Status to describe the net request status

---
+++ home/src/main/java/com/topview/purejoy/home/data/Status.kt
@@ -0,0 +1,8 @@
+package com.topview.purejoy.home.data
+
+/**
+ * 表示数据的获取状态
+ * */
+internal object Status {
+    const val HOME_DISCOVER_BANNER_NET_ERROR = "home_discover_banner_net_error"
+}
--- home/src/main/java/com/topview/purejoy/home/discover/HomeDiscoverViewModel.kt
+++ home/src/main/java/com/topview/purejoy/home/discover/HomeDiscoverViewModel.kt
@@ -4,24 +4,24 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.topview.purejoy.common.mvvm.viewmodel.MVVMViewModel
 import com.topview.purejoy.home.data.Status
-import com.topview.purejoy.home.data.bean.BannerJson
 import com.topview.purejoy.home.data.repo.HomeRepository
+import com.topview.purejoy.home.entity.HomeDiscoverBannerItem
 
 class HomeDiscoverViewModel : MVVMViewModel() {
     private val repository = HomeRepository
 
-    val bannerLiveData: MutableLiveData<List<BannerJson.Banner>> by lazy {
-        MutableLiveData<List<BannerJson.Banner>>()
+    val bannerLiveData: MutableLiveData<List<HomeDiscoverBannerItem>> by lazy {
+        MutableLiveData<List<HomeDiscoverBannerItem>>()
     }
 
     fun getBanners() {
-        viewModelScope.rxLaunch<BannerJson> {
+        viewModelScope.rxLaunch<List<HomeDiscoverBannerItem>> {
             onRequest = {
                 repository.getBanners()
             }
 
             onSuccess = {
-                bannerLiveData.value = it.banners
+                bannerLiveData.value = it
             }
             
             onError = {
