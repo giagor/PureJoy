94c556cebbc5f2ff682a809767fd25b4cc0e1fff
lanlin-code <2529813756@qq.com>
2021-11-13 22:10:23 +0800

    feat(music): daily recommended adapters

--- musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/recommendation/music/adapter/DailyRecommendAdapter.kt
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/recommendation/music/adapter/DailyRecommendAdapter.kt
@@ -1,61 +1,82 @@
 package com.topview.purejoy.musiclibrary.recommendation.music.adapter
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.topview.purejoy.common.widget.RoundImageView
+import com.topview.purejoy.musiclibrary.BR
 import com.topview.purejoy.musiclibrary.R
 import com.topview.purejoy.musiclibrary.common.adapter.CommonAdapter
+import com.topview.purejoy.musiclibrary.common.adapter.DataBindingAdapter
 import com.topview.purejoy.musiclibrary.recommendation.music.entity.DailySongs
 
+@BindingAdapter(value = ["imageUrl", "holder", "error"])
+fun setImageUrl(view: ImageView, imageUrl: String, holder: Drawable?, error: Drawable?) {
+    Glide.with(view.context).asBitmap().load(imageUrl).placeholder(holder).error(error).into(view)
+}
+
 class DailyRecommendAdapter(
-    var dailySongs: DailySongs = DailySongs(
-        dailySongs = mutableListOf(),
-        recommendReasons = mutableListOf())
-) : CommonAdapter<DailyRecommendAdapter.DailyRecommendHolder>() {
-
-    class DailyRecommendHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val layout: LinearLayout = view.findViewById(R.id.music_daily_recommend_item_layout)
-        val imageView: RoundImageView = view.findViewById(R.id.music_daily_recommend_item_iv)
-        val name: TextView = view.findViewById(R.id.music_daily_recommend_item_name_tx)
-        val reason: TextView = view.findViewById(R.id.music_daily_recommend_item_reason_tx)
-        val author: TextView = view.findViewById(R.id.music_daily_recommend_item_author_tx)
-        val button: ImageButton = view.findViewById(R.id.music_daily_recommend_item_more_bt)
+    var dailySongs: DailySongs = DailySongs(dailySongs = mutableListOf(),
+        recommendReasons = mutableListOf())) :
+    DataBindingAdapter<DailyRecommendAdapter.DailyRecommendHolder>() {
+    override fun variableId(viewType: Int): Int {
+        return BR.musicItem
     }
 
-    override fun layoutId(): Int {
-        return R.layout.linearlayout_music_recommend_item
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.layout_music_recommend_item
     }
 
-    override fun createViewHolder(root: View): DailyRecommendHolder {
-        return DailyRecommendHolder(root)
-    }
-
-    override fun onBindViewHolder(holder: DailyRecommendHolder, position: Int) {
-        val item = dailySongs.dailySongs[position]
-        if (position >= dailySongs.recommendReasons.size) {
-            holder.reason.visibility = View.GONE
-        } else {
-            holder.reason.text = dailySongs.recommendReasons[position].reason
-            holder.reason.visibility = View.VISIBLE
-        }
-        Glide.with(holder.itemView.context).asBitmap()
-            .error(R.drawable.white_holder).placeholder(R.drawable.white_holder)
-            .load(item.al.picUrl).into(holder.imageView)
-        holder.name.text = item.al.name
-        holder.author.text = item.getAuthors()
-        holder.layout.setOnClickListener {
-            // 播放当前歌曲
-        }
-        holder.button.setOnClickListener {
-            // 弹窗
-        }
+    override fun getItem(position: Int): Any {
+        return dailySongs.dailySongs[position]
     }
 
     override fun getItemCount(): Int {
         return dailySongs.dailySongs.size
+    }
+
+    override fun onBindViewHolder(holder: DailyRecommendHolder, position: Int) {
+        holder.layout.setOnClickListener {
+            // 播放全部歌曲
+        }
+        holder.button.setOnClickListener {
+            // 弹窗
+        }
+        holder.reasonTx.apply {
+            val s = findReason(dailySongs.dailySongs[position].id)
+            if (s.isEmpty()) {
+                visibility = View.GONE
+            } else {
+                text = s
+                visibility = View.VISIBLE
+            }
+        }
+        super.onBindViewHolder(holder, position)
+    }
+
+    private fun findReason(id: Long): String {
+        for (r in dailySongs.recommendReasons) {
+            if (r.songId == id) {
+                return r.reason
+            }
+        }
+        return ""
+    }
+
+    class DailyRecommendHolder(viewDataBinding: ViewDataBinding) : BindingHolder(viewDataBinding) {
+        val layout = itemView.findViewById<LinearLayout>(R.id.music_daily_recommend_item_layout)
+        val reasonTx = itemView.findViewById<TextView>(R.id.music_daily_recommend_item_reason_tx)
+        val button = itemView.findViewById<ImageButton>(R.id.music_daily_recommend_item_more_bt)
+    }
+
+    override fun createHolder(viewBinding: ViewDataBinding): DailyRecommendHolder {
+        return DailyRecommendHolder(viewBinding)
     }
 }
