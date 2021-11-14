74a28ec274244076e07bbcdd1c3cc5ad20cd60f4
18822835767 <1165407654@qq.com>
2021-11-14 21:03:59 +0800

    feat(home): Finish coding about getting data of daily recommend playlist

--- home/src/main/java/com/topview/purejoy/home/data/api/HomeService.kt
+++ home/src/main/java/com/topview/purejoy/home/data/api/HomeService.kt
@@ -1,6 +1,7 @@
 package com.topview.purejoy.home.data.api
 
 import com.topview.purejoy.home.data.bean.BannerJson
+import com.topview.purejoy.home.data.bean.DailyRecommendPlayListJson
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,4 +9,7 @@ import retrofit2.http.Query
 interface HomeService {
     @GET("/banner")
     fun getBanners(@Query("type") type: Int): Call<BannerJson>
+
+    @GET("/personalized")
+    fun getDailyRecommendPlayList(@Query("limit") limit: Int): Call<DailyRecommendPlayListJson>
 }
--- home/src/main/java/com/topview/purejoy/home/data/repo/HomeRepository.kt
+++ home/src/main/java/com/topview/purejoy/home/data/repo/HomeRepository.kt
@@ -2,6 +2,7 @@ package com.topview.purejoy.home.data.repo
 
 import com.topview.purejoy.home.data.source.HomeLocalStore
 import com.topview.purejoy.home.data.source.HomeRemoteStore
+import com.topview.purejoy.home.entity.DailyRecommendPlayList
 import com.topview.purejoy.home.entity.HomeDiscoverBannerItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -13,4 +14,9 @@ internal object HomeRepository {
     suspend fun getBanners(): List<HomeDiscoverBannerItem>? = withContext(Dispatchers.IO) {
         homeRemoteStore.getBanners()
     }
+
+    suspend fun getDailyRecommendPlayList(limit: Int = 6): List<DailyRecommendPlayList>? =
+        withContext(Dispatchers.IO) {
+            homeRemoteStore.getDailyRecommendPlayList(limit)
+        }
 }
