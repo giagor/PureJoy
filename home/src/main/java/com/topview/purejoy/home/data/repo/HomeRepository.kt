de9213244a8415d6c6930e1f227afcc7d11de3b1
18822835767 <1165407654@qq.com>
2021-11-16 12:42:57 +0800

    feat(home): Finishing coding about getRecommendNewSong method in HomeRepository.kt

--- home/src/main/java/com/topview/purejoy/home/data/repo/HomeRepository.kt
+++ home/src/main/java/com/topview/purejoy/home/data/repo/HomeRepository.kt
@@ -4,6 +4,7 @@ import com.topview.purejoy.home.data.source.HomeLocalStore
 import com.topview.purejoy.home.data.source.HomeRemoteStore
 import com.topview.purejoy.home.entity.DailyRecommendPlayList
 import com.topview.purejoy.home.entity.HomeDiscoverBannerItem
+import com.topview.purejoy.home.entity.RecommendNewSong
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -18,5 +19,10 @@ internal object HomeRepository {
     suspend fun getDailyRecommendPlayList(limit: Int = 6): List<DailyRecommendPlayList>? =
         withContext(Dispatchers.IO) {
             homeRemoteStore.getDailyRecommendPlayList(limit)
+        }
+
+    suspend fun getRecommendNewSong(limit: Int = 12): List<RecommendNewSong>? =
+        withContext(Dispatchers.IO) {
+            homeRemoteStore.getRecommendNewSong(limit)
         }
 }
