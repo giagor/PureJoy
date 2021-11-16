5e34ae006f49bbe9215aaf33714b99026808e10e
18822835767 <1165407654@qq.com>
2021-11-16 12:39:10 +0800

    feat(home): Add the api to get RecommendNewSongJson data

--- home/src/main/java/com/topview/purejoy/home/data/api/HomeService.kt
+++ home/src/main/java/com/topview/purejoy/home/data/api/HomeService.kt
@@ -2,6 +2,7 @@ package com.topview.purejoy.home.data.api
 
 import com.topview.purejoy.home.data.bean.BannerJson
 import com.topview.purejoy.home.data.bean.DailyRecommendPlayListJson
+import com.topview.purejoy.home.data.bean.RecommendNewSongJson
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,4 +13,7 @@ interface HomeService {
 
     @GET("/personalized")
     fun getDailyRecommendPlayList(@Query("limit") limit: Int): Call<DailyRecommendPlayListJson>
+
+    @GET("/personalized/newsong")
+    fun getRecommendNewSong(@Query("limit") limit: Int): Call<RecommendNewSongJson>
 }
