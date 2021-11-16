60d7d7b4fe62c3967a2f6935b5fb2aab4a0ba220
lanlin-code <2529813756@qq.com>
2021-11-16 16:00:22 +0800

    feat(music): create PlayingActivity

---
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/playing/view/PlayingActivity.kt
@@ -0,0 +1,15 @@
+package com.topview.purejoy.musiclibrary.playing.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.topview.purejoy.musiclibrary.R
+
+class PlayingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playing_layout)
+    }
+
+
+}
