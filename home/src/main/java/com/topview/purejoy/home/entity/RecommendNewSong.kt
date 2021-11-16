34ead6dddee9b6f1a5efda361df81fd45b60c7fd
18822835767 <1165407654@qq.com>
2021-11-16 12:37:48 +0800

    feat(home): Add RecommendNewSong.kt

---
+++ home/src/main/java/com/topview/purejoy/home/entity/RecommendNewSong.kt
@@ -0,0 +1,14 @@
+package com.topview.purejoy.home.entity
+
+/**
+ * id：歌曲的id
+ * name：歌曲的名字
+ * picUrl：歌曲的封面图
+ * artistName：歌手名字
+ * */
+data class RecommendNewSong(
+    var id: Long?,
+    var name: String? = null,
+    var picUrl: String? = null,
+    var artistName: String? = null
+)
