5c2f3e3e3ae69d8091c6697e9080553fbfb0da68
18822835767 <1165407654@qq.com>
2021-11-18 19:23:25 +0800

    feat(home): Add the adapter of RecommendNewSong item

--- home/src/main/java/com/topview/purejoy/home/discover/adapter/RecommendNewSongAdapter.kt
+++ home/src/main/java/com/topview/purejoy/home/discover/adapter/RecommendNewSongAdapter.kt
@@ -1,15 +1,34 @@
 package com.topview.purejoy.home.discover.adapter
 
+import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.topview.purejoy.common.util.getWindowWidth
 import com.topview.purejoy.home.R
 import com.topview.purejoy.home.databinding.ItemHomeRecommendNewSongBinding
 import com.topview.purejoy.home.entity.RecommendNewSong
+import com.topview.purejoy.home.util.Common.DISCOVER_RECOMMEND_NEW_SONG_ITEM_WIDTH_SMALLER_THAN_SCREEN
 
 class RecommendNewSongAdapter :
     BaseQuickAdapter<RecommendNewSong, BaseDataBindingHolder<ItemHomeRecommendNewSongBinding>>(
         R.layout.item_home_recommend_new_song
     ) {
+
+    override fun onItemViewHolderCreated(
+        viewHolder: BaseDataBindingHolder<ItemHomeRecommendNewSongBinding>,
+        viewType: Int
+    ) {
+        val binding: ItemHomeRecommendNewSongBinding? = viewHolder.dataBinding
+        binding?.let {
+            // 获取原来的View的Root的布局参数
+            val sourcePrams = it.root.layoutParams
+            // 使得item的宽度比屏幕宽度小一些
+            it.root.layoutParams = RecyclerView.LayoutParams(sourcePrams).apply {
+                width =
+                    getWindowWidth(it.root.context) - DISCOVER_RECOMMEND_NEW_SONG_ITEM_WIDTH_SMALLER_THAN_SCREEN
+            }
+        }
+    }
 
     override fun convert(
         holder: BaseDataBindingHolder<ItemHomeRecommendNewSongBinding>,
