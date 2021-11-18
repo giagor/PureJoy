9db223ae6111481cb607614494217dc37b73365f
18822835767 <1165407654@qq.com>
2021-11-18 19:18:16 +0800

    feat(common): Add a overload method about dpTpPx

--- common/src/main/java/com/topview/purejoy/common/util/ViewUtil.kt
+++ common/src/main/java/com/topview/purejoy/common/util/ViewUtil.kt
@@ -9,3 +9,10 @@ fun dpToPx(dp: Float): Float =
         dp,
         Resources.getSystem().displayMetrics
     )
+
+fun dpToPx(dp: Int): Int =
+    TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp.toFloat(),
+        Resources.getSystem().displayMetrics
+    ).toInt()
