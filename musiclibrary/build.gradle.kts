be0753c3b4ea7cbd917a9880409f824a97973cbe
usagi <1023192250@qq.com>
2021-11-21 02:24:53 +0800

    refactor: According to the dependency constant, migrate the gradle script using groovy syntax to kotlin DSL

---
+++ musiclibrary/build.gradle.kts
@@ -0,0 +1,46 @@
+import android.annotation.SuppressLint
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("com.android.support:palette-v7:28.0.0")
+    implementation(project(":common"))
+    implementation(project(":dependencies"))
+    testImplementation(TestDeps.Local.junit)
+    androidTestImplementation(TestDeps.Instrumentation.espresso)
+    androidTestImplementation(TestDeps.Instrumentation.junitExtension)
+}
