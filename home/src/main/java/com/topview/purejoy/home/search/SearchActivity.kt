431de8669c8db68ecec0305e727ec7657ef19109
18822835767 <1165407654@qq.com>
2021-11-23 21:01:34 +0800

    feat(home): Create search activity

---
+++ home/src/main/java/com/topview/purejoy/home/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.topview.purejoy.home.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.topview.purejoy.home.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_search)
+    }
+}
