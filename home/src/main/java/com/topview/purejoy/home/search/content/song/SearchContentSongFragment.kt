fa0b3da3a4ef34de1e22839c87417e3d0decdf66
18822835767 <1165407654@qq.com>
2021-11-24 16:25:17 +0800

    feat(home): Add SearchContentSongFragment to show songs searched

---
+++ home/src/main/java/com/topview/purejoy/home/search/content/song/SearchContentSongFragment.kt
@@ -0,0 +1,29 @@
+package com.topview.purejoy.home.search.content.song
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.topview.purejoy.home.R
+
+class SearchContentSongFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_search_content_song, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SearchContentSongFragment()
+    }
+}
