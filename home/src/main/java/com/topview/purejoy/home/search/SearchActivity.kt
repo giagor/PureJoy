ef914d4c15981819eb0847f81da18d2fc4da62e2
18822835767 <1165407654@qq.com>
2021-11-24 16:02:52 +0800

    feat(home): Add tabLayout in SearchContentTabFragment

--- home/src/main/java/com/topview/purejoy/home/search/SearchActivity.kt
+++ home/src/main/java/com/topview/purejoy/home/search/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.widget.SearchView
 import com.topview.purejoy.common.base.CommonActivity
 import com.topview.purejoy.home.R
+import com.topview.purejoy.home.search.content.SearchContentTabFragment
 
 class SearchActivity : CommonActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,7 @@ class SearchActivity : CommonActivity() {
 
         val searchView: SearchView = findViewById(R.id.search_view)
         searchView.onActionViewExpanded()
+        addFragment(R.id.home_fl_fragment_layout, SearchContentTabFragment.newInstance())
     }
 
     override fun getLayoutId(): Int {
