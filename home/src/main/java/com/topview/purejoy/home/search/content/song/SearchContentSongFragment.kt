a0bdf15fa987321c11c14c60886c52b81e3d6a1e
18822835767 <1165407654@qq.com>
2021-11-25 20:03:02 +0800

    feat(home): Observe liveData in SearchContentSongFragment

--- home/src/main/java/com/topview/purejoy/home/search/content/song/SearchContentSongFragment.kt
+++ home/src/main/java/com/topview/purejoy/home/search/content/song/SearchContentSongFragment.kt
@@ -1,24 +1,41 @@
 package com.topview.purejoy.home.search.content.song
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.topview.purejoy.common.mvvm.fragment.MVVMFragment
 import com.topview.purejoy.home.R
+import com.topview.purejoy.home.databinding.FragmentHomeSearchContentSongBinding
+import com.topview.purejoy.home.util.getAndroidViewModelFactory
 
-class SearchContentSongFragment : Fragment() {
+class SearchContentSongFragment :
+    MVVMFragment<SearchContentSongViewModel, FragmentHomeSearchContentSongBinding>() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+        observe()
+        initData()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_search_content_song, container, false)
+    override fun getLayoutId(): Int = R.layout.fragment_home_search_content_song
+
+    override fun getViewModelClass(): Class<SearchContentSongViewModel> =
+        SearchContentSongViewModel::class.java
+
+    override fun createFactory(): ViewModelProvider.Factory = getAndroidViewModelFactory()
+
+    private fun initData() {
+
+    }
+
+    private fun observe() {
+        viewModel.searchSongCountLiveData.observe(viewLifecycleOwner, {
+
+        })
+
+        viewModel.searchSongsLiveData.observe(viewLifecycleOwner, {
+        })
     }
 
     companion object {
