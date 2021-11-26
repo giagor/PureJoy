26b45b78f9241334b170b69efd37d9331b1dd5eb
18822835767 <1165407654@qq.com>
2021-11-26 14:43:42 +0800

    feat(home): Coding the BindingAdapters of searched songs

---
+++ home/src/main/java/com/topview/purejoy/home/search/content/song/adapter/binding/SearchSongBindingAdapters.kt
@@ -0,0 +1,19 @@
+package com.topview.purejoy.home.search.content.song.adapter.binding
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.topview.purejoy.home.entity.Song
+import com.topview.purejoy.home.search.content.song.adapter.SearchContentSongAdapter
+
+@BindingAdapter("searchSongAdapter")
+fun setSearchContentSongAdapter(recyclerView: RecyclerView, adapter: SearchContentSongAdapter) {
+    recyclerView.adapter = adapter
+}
+
+@BindingAdapter("searchSongs")
+fun setSearchSongs(recyclerView: RecyclerView, list: List<Song>?) {
+    list?.let {
+        val adapter = recyclerView.adapter as SearchContentSongAdapter
+        adapter.setList(list)
+    }
+}
