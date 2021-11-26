5d1b25595aabc985d70c181ab0c3bcaf84e96821
18822835767 <1165407654@qq.com>
2021-11-26 14:43:16 +0800

    feat(home): Coding the adapter of searched songs

---
+++ home/src/main/java/com/topview/purejoy/home/search/content/song/adapter/SearchContentSongAdapter.kt
@@ -0,0 +1,20 @@
+package com.topview.purejoy.home.search.content.song.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.topview.purejoy.home.R
+import com.topview.purejoy.home.databinding.ItemHomeSearchSongBinding
+import com.topview.purejoy.home.entity.Song
+
+class SearchContentSongAdapter :
+    BaseQuickAdapter<Song, BaseDataBindingHolder<ItemHomeSearchSongBinding>>(R.layout.item_home_search_song) {
+
+    override fun convert(holder: BaseDataBindingHolder<ItemHomeSearchSongBinding>, item: Song) {
+        // 获取Binding
+        val binding: ItemHomeSearchSongBinding? = holder.dataBinding
+        binding?.let {
+            // 设置数据
+            it.searchSong = item
+        }
+    }
+}
