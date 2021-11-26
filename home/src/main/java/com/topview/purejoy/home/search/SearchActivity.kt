e3238c4269c8593fee9a1b94741fdb9e2904c597
18822835767 <1165407654@qq.com>
2021-11-26 15:08:16 +0800

    feat(home): Make SearchActivity extend BindingActivity

--- home/src/main/java/com/topview/purejoy/home/search/SearchActivity.kt
+++ home/src/main/java/com/topview/purejoy/home/search/SearchActivity.kt
@@ -2,15 +2,17 @@ package com.topview.purejoy.home.search
 
 import android.os.Bundle
 import androidx.appcompat.widget.SearchView
-import com.topview.purejoy.common.base.CommonActivity
+import com.topview.purejoy.common.base.binding.BindingActivity
 import com.topview.purejoy.home.R
+import com.topview.purejoy.home.databinding.ActivityHomeSearchBinding
+import com.topview.purejoy.home.databinding.HomeActivityHomeBinding
 import com.topview.purejoy.home.search.content.tab.SearchContentTabFragment
 
-class SearchActivity : CommonActivity() {
+class SearchActivity : BindingActivity<ActivityHomeSearchBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val searchView: SearchView = findViewById(R.id.search_view)
+        val searchView: SearchView = binding.homeSvBox
         searchView.onActionViewExpanded()
         addFragment(R.id.home_fl_fragment_layout, SearchContentTabFragment.newInstance())
     }
