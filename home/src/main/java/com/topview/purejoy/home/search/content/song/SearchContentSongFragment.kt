d5096129bd1dc9e07fff0944432042a4b7e2af7f
18822835767 <1165407654@qq.com>
2021-11-26 14:44:28 +0800

    feat(home): Init recyclerView in search song fragment

--- home/src/main/java/com/topview/purejoy/home/search/content/song/SearchContentSongFragment.kt
+++ home/src/main/java/com/topview/purejoy/home/search/content/song/SearchContentSongFragment.kt
@@ -3,9 +3,11 @@ package com.topview.purejoy.home.search.content.song
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.topview.purejoy.common.mvvm.fragment.MVVMFragment
 import com.topview.purejoy.home.R
 import com.topview.purejoy.home.databinding.FragmentHomeSearchContentSongBinding
+import com.topview.purejoy.home.search.content.song.adapter.SearchContentSongAdapter
 import com.topview.purejoy.home.util.getAndroidViewModelFactory
 
 class SearchContentSongFragment :
@@ -14,6 +16,7 @@ class SearchContentSongFragment :
         super.onViewCreated(view, savedInstanceState)
 
         binding.viewModel = viewModel
+        initRecyclerView()
         observe()
         initData()
     }
@@ -26,7 +29,14 @@ class SearchContentSongFragment :
     override fun createFactory(): ViewModelProvider.Factory = getAndroidViewModelFactory()
 
     private fun initData() {
+        viewModel.getSearchSongByFirst("光辉岁月")
+    }
 
+    private fun initRecyclerView() {
+        val layoutManager = LinearLayoutManager(requireContext())
+        val adapter = SearchContentSongAdapter()
+        binding.searchContentSongLayoutManager = layoutManager
+        binding.searchContentSongAdapter = adapter
     }
 
     private fun observe() {
