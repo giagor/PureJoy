9f351cec5f4275bfe27da3a76bc24a1165e2fffc
18822835767 <1165407654@qq.com>
2021-11-30 23:54:59 +0800

    feat(home): Add songCounts field in PlayList bean

--- home/src/main/java/com/topview/purejoy/home/entity/PlayList.kt
+++ home/src/main/java/com/topview/purejoy/home/entity/PlayList.kt
@@ -2,10 +2,15 @@ package com.topview.purejoy.home.entity
 
 /**
  * 每日推荐歌单对应的实体类
+ *
+ * picUrl：歌单的封面
+ * playCount：播放数量
+ * songCounts：包含的歌曲数量
  * */
 data class PlayList(
     var id: Long? = null,
     var name: String? = null,
     var picUrl: String? = null,
-    var playCount: Long? = null
+    var playCount: Long? = null,
+    var songCounts: Int? = null
 ) 
