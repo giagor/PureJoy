53784d328af45b4818783cc2b4215487a7c00041
usagi <1023192250@qq.com>
2021-12-03 17:34:33 +0800

    feat(video): add video module

---
+++ video/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = Build.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Build.minimumSdkVersion
+        targetSdk = Build.targetSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Deps.Compose.version
+    }
+}
+
+dependencies {
+
+    testImplementation(TestDeps.Local.junit)
+    androidTestImplementation(TestDeps.Instrumentation.espresso)
+    androidTestImplementation(TestDeps.Instrumentation.junitExtension)
+}
