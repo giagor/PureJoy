762095d38b24527f0cbf8ad6385d94e125339087
lanlin-code <2529813756@qq.com>
2021-12-07 20:52:22 +0800

    feat(music): store player's state with sp

---
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/common/util/SP.kt
@@ -0,0 +1,21 @@
+package com.topview.purejoy.musiclibrary.common.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.topview.purejoy.common.app.CommonApplication
+
+class SP {
+    companion object {
+
+        const val SP_NAME = "music"
+
+        val sp by lazy {
+            getSharePreference(SP_NAME)
+        }
+
+        fun getSharePreference(name: String): SharedPreferences {
+            return CommonApplication.getContext().getSharedPreferences(name, Context.MODE_PRIVATE)
+        }
+
+    }
+}
