d36c1de5608fa478e4a640f42ff9654c9cae9b62
lanlin-code <2529813756@qq.com>
2021-12-07 20:54:57 +0800

    feat(music): convert music entities to database table mapped entities

--- musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/entity/MusicItem.kt
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/entity/MusicItem.kt
@@ -2,6 +2,10 @@ package com.topview.purejoy.musiclibrary.entity
 
 import com.topview.purejoy.musiclibrary.data.Item
 import com.topview.purejoy.musiclibrary.data.Wrapper
+import com.topview.purejoy.musiclibrary.service.recover.db.entity.RecoverALData
+import com.topview.purejoy.musiclibrary.service.recover.db.entity.RecoverARData
+import com.topview.purejoy.musiclibrary.service.recover.db.entity.RecoverData
+import com.topview.purejoy.musiclibrary.service.recover.db.entity.RecoverMusicData
 import java.io.Serializable
 
 data class AR(val id: Long, val name: String = "") : Serializable
@@ -18,10 +22,7 @@ class MusicItem(
     val id: Long,
     var url: String? = null,
     val ar: List<AR> = listOf(),
-    val al: AL,
-    val h: BR,
-    val m: BR,
-    val l: BR) : Item {
+    val al: AL, ) : Item {
 
     override fun url(): String? {
         return url
@@ -53,6 +54,35 @@ class MusicItem(
     override fun toString(): String {
         return "[MusicItem name = $name, id = $id, url = $url, al = $al, ar = $ar]"
     }
+}
+
+fun MusicItem.toRecoverMusic(): RecoverMusicData {
+    return RecoverMusicData(id = id, name = name, url = url)
+}
+
+fun AR.toRecoverAR(mid: Long): RecoverARData {
+    return RecoverARData(id = id, name = name, mid = mid)
+}
+
+fun AL.toRecoverAL(mid: Long): RecoverALData {
+    return RecoverALData(id = id, name = name, picUrl = picUrl, mid = mid)
+}
+
+fun RecoverData.toMusicItem(): MusicItem {
+    val al = alData.toAL()
+    val ar = mutableListOf<AR>()
+    for (d in arDataList) {
+        ar.add(d.toAR())
+    }
+    return MusicItem(name = musicData.name, id = musicData.id, url = musicData.url, ar = ar, al = al)
+}
+
+fun RecoverARData.toAR(): AR {
+    return AR(id, name)
+}
+
+fun RecoverALData.toAL(): AL {
+    return AL(id, name, picUrl)
 }
 
 /**
