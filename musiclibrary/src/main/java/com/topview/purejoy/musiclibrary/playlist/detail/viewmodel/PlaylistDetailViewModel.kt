812a3d246eb7a345684d711c1e5f50db0dfc0eb7
lanlin-code <2529813756@qq.com>
2021-12-08 23:42:13 +0800

    feat(music): Request playlist details

---
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/playlist/detail/viewmodel/PlaylistDetailViewModel.kt
@@ -0,0 +1,58 @@
+package com.topview.purejoy.musiclibrary.playlist.detail.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.topview.purejoy.common.mvvm.viewmodel.MVVMViewModel
+import com.topview.purejoy.common.net.await
+import com.topview.purejoy.musiclibrary.entity.MusicResponse
+import com.topview.purejoy.musiclibrary.playlist.detail.repo.IPlaylistDetailRepository
+import com.topview.purejoy.musiclibrary.playlist.detail.repo.PlaylistDetailRepository
+import com.topview.purejoy.musiclibrary.playlist.entity.PlaylistResponse
+import java.lang.StringBuilder
+
+class PlaylistDetailViewModel(
+    private val repo: IPlaylistDetailRepository = PlaylistDetailRepository(),
+    val response: MutableLiveData<PlaylistResponse?> = MutableLiveData(),
+    val songsResponse: MutableLiveData<MusicResponse?> = MutableLiveData()
+) : MVVMViewModel() {
+
+    private val TAG = "PlaylistVM"
+
+    fun getDetails(id: Long) {
+        viewModelScope.rxLaunch<PlaylistResponse> {
+            onRequest = {
+                repo.getDetails(id).await()
+            }
+            onError = {
+                response.postValue(null)
+            }
+            onSuccess = {
+                response.postValue(it)
+                val builder = StringBuilder()
+                for (i in it.playlist.trackIds) {
+                    if (builder.isNotEmpty()) {
+                        builder.append(',')
+                    }
+                    builder.append(i.id)
+                }
+                requestSongsDetails(builder.toString())
+            }
+        }
+    }
+
+    fun requestSongsDetails(ids: String) {
+        viewModelScope.rxLaunch<MusicResponse> {
+            onRequest = {
+                repo.requestSongDetails(ids).await()
+            }
+            onError = {
+                songsResponse.postValue(null)
+            }
+            onSuccess = {
+                songsResponse.postValue(it)
+            }
+        }
+    }
+
+}
