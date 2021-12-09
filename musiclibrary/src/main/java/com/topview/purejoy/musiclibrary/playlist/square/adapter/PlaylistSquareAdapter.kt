66f77aeeabd29df115a5718469063bd5a950da96
lanlin-code <2529813756@qq.com>
2021-12-09 21:38:15 +0800

    fix(music): fix reflect error

--- musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/playlist/square/adapter/PlaylistSquareAdapter.kt
+++ musiclibrary/src/main/java/com/topview/purejoy/musiclibrary/playlist/square/adapter/PlaylistSquareAdapter.kt
@@ -1,33 +1,21 @@
 package com.topview.purejoy.musiclibrary.playlist.square.adapter
 
-import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.databinding.ViewDataBinding
-import coil.ImageLoader
+import android.widget.TextView
 import coil.load
-import coil.size.Scale
 import coil.transform.RoundedCornersTransformation
-import com.topview.purejoy.common.widget.compose.RoundedCornerImageView
-import com.topview.purejoy.musiclibrary.BR
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.topview.purejoy.musiclibrary.R
-import com.topview.purejoy.musiclibrary.common.adapter.CommonBindingAdapter
 import com.topview.purejoy.musiclibrary.common.adapter.DataClickListener
 import com.topview.purejoy.musiclibrary.playlist.entity.Playlist
 
-class PlaylistSquareAdapter : CommonBindingAdapter<Playlist,
-        PlaylistSquareAdapter.PlaylistSquareHolder>(layoutId = R.layout.playlist_square_item) {
+class PlaylistSquareAdapter : BaseQuickAdapter<Playlist,
+        BaseViewHolder>(R.layout.playlist_square_item) {
 
     var itemClickListener: DataClickListener<Playlist>? = null
 
-    class PlaylistSquareHolder(binding: ViewDataBinding) : CommonBindingAdapter.BindingHolder(binding) {
-        override fun variableId(): Int {
-           return BR.squarePlaylist
-        }
-
-    }
-
-
-    override fun convert(holder: PlaylistSquareHolder, item: Playlist) {
+    override fun convert(holder: BaseViewHolder, item: Playlist) {
         holder.itemView.setOnClickListener {
             itemClickListener?.onClick(item, data.indexOf(item))
         }
@@ -35,15 +23,8 @@ class PlaylistSquareAdapter : CommonBindingAdapter<Playlist,
         iv.load(item.coverImgUrl) {
             transformations(RoundedCornersTransformation(radius = 16f))
         }
-        super.convert(holder, item)
+        holder.getView<TextView>(R.id.playlist_square_item_name_tx).text = item.name
     }
 
-    override fun createBindingHolder(
-        parent: ViewGroup,
-        layoutResId: Int,
-        binding: ViewDataBinding
-    ): PlaylistSquareHolder {
-        return PlaylistSquareHolder(binding)
-    }
 
 }
