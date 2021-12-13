01479370ac2df570313671faf8fa7319b4bc4195
usagi <1023192250@qq.com>
2021-12-14 00:17:52 +0800

    feat(video): add VideoActivity and VideoViewModel, also update the manifest

---
+++ video/src/main/java/com/topview/purejoy/video/VideoActivity.kt
@@ -0,0 +1,101 @@
+package com.topview.purejoy.video
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.*
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.topview.purejoy.common.base.ComposeActivity
+import com.topview.purejoy.common.util.StatusBarUtil
+import com.topview.purejoy.common.util.StatusBarUtil.setAutoFitSystemWindows
+import com.topview.purejoy.common.util.StatusBarUtil.setStatusBarBackground
+import com.topview.purejoy.common.util.StatusBarUtil.setStatusBarTextColor
+import com.topview.purejoy.video.ui.LocalExoPlayer
+import com.topview.purejoy.video.ui.horizontal.HorizontalVideoScreen
+import com.topview.purejoy.video.ui.state.VideoLoadState
+import com.topview.purejoy.video.util.RouterParamsConstant
+
+class VideoActivity: ComposeActivity() {
+
+    private val viewModel by viewModels<VideoViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                    if (modelClass == VideoViewModel::class.java) {
+                        return VideoViewModel(
+                            intent.extras?.getParcelableArrayList(
+                                RouterParamsConstant.VIDEO_ARRAYLIST)!!
+                        ) as T
+                    } else {
+                        throw IllegalArgumentException("Illegal ViewModel Class")
+                    }
+                }
+            }
+        }
+    )
+
+    override fun setStatusBarStyle() {
+        window.setAutoFitSystemWindows(false)
+            .setStatusBarBackground(Color.BLACK)
+            .setStatusBarTextColor(false)
+        window.navigationBarColor = Color.BLACK
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProvideWindowInsets(consumeWindowInsets = false) {
+                val videoItem = viewModel.getPagingVideoFlow().collectAsLazyPagingItems()
+                val videoLoadState by viewModel.videoLoadState.collectAsState()
+
+                // 注册生命周期回调
+                LocalLifecycleOwner.current.lifecycle.addObserver(object : LifecycleObserver {
+                    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+                    fun resumeVideo() {
+                        viewModel.play()
+                    }
+
+                    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+                    fun pauseVideo() {
+                        viewModel.pause()
+                    }
+                })
+
+                CompositionLocalProvider(LocalExoPlayer provides viewModel.exoPlayer) {
+                    HorizontalVideoScreen(
+                        items = videoItem,
+                        videoLoadState = videoLoadState,
+                        onPageChange = {
+                            viewModel.onPageChange(it)
+                        },
+                        onVideoSurfaceClick = {
+                            if (videoLoadState is VideoLoadState.Playing) {
+                                viewModel.pause()
+                            } else if (videoLoadState is VideoLoadState.Pause) {
+                                viewModel.play()
+                            }
+                        },
+                        onRetryClick = viewModel::reloadVideo,
+                        onBackClick = {
+                            finish()
+                        },
+                    )
+                }
+            }
+        }
+        val composeView = window.decorView
+            .findViewById<ViewGroup>(android.R.id.content)
+            .getChildAt(0) as? ComposeView
+        composeView?.let {
+            StatusBarUtil.fitSystemBar(it)
+        }
+    }
+}
---
+++ video/src/main/java/com/topview/purejoy/video/VideoViewModel.kt
@@ -0,0 +1,117 @@
+package com.topview.purejoy.video
+
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.topview.purejoy.common.app.CommonApplication
+import com.topview.purejoy.common.mvvm.viewmodel.MVVMViewModel
+import com.topview.purejoy.video.data.repo.VideoRepository
+import com.topview.purejoy.video.entity.Video
+import com.topview.purejoy.video.ui.state.VideoLoadState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flow
+import java.util.*
+
+class VideoViewModel(initialList: ArrayList<Video>): MVVMViewModel(), Player.Listener {
+
+    private val repository = VideoRepository(initialList)
+
+    val exoPlayer = ExoPlayer.Builder(CommonApplication.getContext()).build()
+
+    private val _videoLoadState: MutableStateFlow<VideoLoadState> = MutableStateFlow(VideoLoadState.Loading)
+
+    val videoLoadState: StateFlow<VideoLoadState> = _videoLoadState
+
+    /**
+     * 每个1000ms便发送video当前的进度的flow
+     */
+    val progressFlow = flow {
+        while (true) {
+            _videoLoadState.value.run {
+                // 只有在状态为正在播放才更新进度
+                if (this is VideoLoadState.Playing) {
+                    emit(exoPlayer.currentPosition)
+                }
+            }
+            delay(1000L)
+        }
+    }.conflate()
+
+
+    init {
+        exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
+        exoPlayer.addListener(this)
+    }
+
+    override fun onPlayerError(error: PlaybackException) {
+        super.onPlayerError(error)
+        _videoLoadState.value = VideoLoadState.Error
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+        when(playbackState) {
+            Player.STATE_BUFFERING -> {
+                _videoLoadState.value = VideoLoadState.Loading
+            }
+            Player.STATE_READY -> {
+                // Loading -> Playing
+                _videoLoadState.value = VideoLoadState.Playing
+                exoPlayer.play()
+            }
+            else -> {}
+        }
+    }
+
+    fun seekTo(milliSecond: Long) {
+        exoPlayer.seekTo(milliSecond)
+    }
+
+    /**
+     * 暂停视频播放
+     * 状态跳转: Playing->Pause
+     */
+    fun pause() {
+        if (_videoLoadState.value is VideoLoadState.Playing) {
+            _videoLoadState.value = VideoLoadState.Pause
+            exoPlayer.pause()
+        }
+    }
+
+    /**
+     * 继续视频播放。
+     * 状态跳转: Pause->Playing
+     */
+    fun play() {
+        if (_videoLoadState.value is VideoLoadState.Pause) {
+            _videoLoadState.value = VideoLoadState.Playing
+            exoPlayer.play()
+        }
+    }
+
+    /**
+     * 当页面切换时触发的处理函数。这个函数在首次加载到第一页的时候也可以触发
+     */
+    fun onPageChange(video: Video?) {
+        video?.apply {
+            exoPlayer.setMediaItem(MediaItem.fromUri(video.videoUrl))
+            exoPlayer.prepare()
+        }
+        if (_videoLoadState.value !is VideoLoadState.Loading) {
+            _videoLoadState.value = VideoLoadState.Loading
+        }
+    }
+
+    /**
+     * 重新加载的处理函数
+     */
+    fun reloadVideo(video: Video?) {
+        onPageChange(video)
+    }
+
+    fun getPagingVideoFlow() = repository.getPagingVideoFlow()
+}
