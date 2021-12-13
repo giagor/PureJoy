c179b2a8948951410e68dab77f6ccc12e9a2c137
usagi <1023192250@qq.com>
2021-12-14 00:10:06 +0800

    feat(video): add HorizontalBottomSlider and it's state

---
+++ video/src/main/java/com/topview/purejoy/video/ui/state/BottomSliderState.kt
@@ -0,0 +1,9 @@
+package com.topview.purejoy.video.ui.state
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class BottomSliderState {
+    var progress: Float by mutableStateOf(0F)
+}
