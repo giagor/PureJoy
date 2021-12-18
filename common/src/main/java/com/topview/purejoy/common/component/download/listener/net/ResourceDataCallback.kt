dfa4c909d69cc881c39f0e48bde562022e2a9717
18822835767 <1165407654@qq.com>
2021-12-18 11:23:15 +0800

    feat(common): Define interfaces to monitor status of network request

---
+++ common/src/main/java/com/topview/purejoy/common/component/download/listener/net/ResourceDataCallback.kt
@@ -0,0 +1,20 @@
+package com.topview.purejoy.common.component.download.listener.net
+
+import java.io.InputStream
+
+/**
+ * 获取资源实体信息时的回调接口
+ * */
+interface ResourceDataCallback {
+    /**
+     * 成功获取
+     *
+     * @param inputStream 资源的输入流
+     * */
+    fun onSuccess(inputStream: InputStream)
+
+    /**
+     * 获取失败
+     * */
+    fun onFailure(t: Throwable)
+}
