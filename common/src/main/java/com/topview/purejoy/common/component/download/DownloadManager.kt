273394d811294c9ee858d6f02133e9675914af42
18822835767 <1165407654@qq.com>
2021-12-19 11:03:14 +0800

    feat(common): 在DownloadManager添加init方法

--- common/src/main/java/com/topview/purejoy/common/component/download/DownloadManager.kt
+++ common/src/main/java/com/topview/purejoy/common/component/download/DownloadManager.kt
@@ -1,13 +1,17 @@
 package com.topview.purejoy.common.component.download
 
+import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import androidx.room.Room
 import com.topview.purejoy.common.component.download.configure.DefaultDownloadConfiguration
 import com.topview.purejoy.common.component.download.configure.DownloadConfiguration
 import com.topview.purejoy.common.component.download.dispatcher.DownloadDispatcher
 import com.topview.purejoy.common.component.download.listener.user.UserDownloadListener
 import com.topview.purejoy.common.component.download.network.DownloadHttpHelper
 import com.topview.purejoy.common.component.download.network.DownloadHttpHelperImpl
+import com.topview.purejoy.common.component.download.storage.DbManager
+import com.topview.purejoy.common.component.download.storage.db.DownloadDataBase
 import com.topview.purejoy.common.component.download.storage.helper.DownloadDbHelper
 import com.topview.purejoy.common.component.download.storage.helper.DownloadDbHelperImpl
 import com.topview.purejoy.common.component.download.task.BatchDownloadTask
@@ -21,12 +25,41 @@ import com.topview.purejoy.common.component.download.util.getDownloadPath
  * */
 // todo 使用方式
 object DownloadManager {
+
+    /**
+     * 表示是否已经初始化
+     * */
+    private var init = false
+
     internal val handler: Handler = Handler(Looper.getMainLooper())
-    var downloadConfiguration: DownloadConfiguration = DefaultDownloadConfiguration()
+    internal var downloadConfiguration: DownloadConfiguration = DefaultDownloadConfiguration()
     internal val downHttpHelper: DownloadHttpHelper =
         DownloadHttpHelperImpl(downloadConfiguration.getDownloadOkClient())
     internal val downDbHelper: DownloadDbHelper = DownloadDbHelperImpl()
     internal val downloadDispatcher: DownloadDispatcher = DownloadDispatcher()
+
+    /**
+     * 初始化方法
+     *
+     * @param applicationContext 通常是Application context
+     * @param configuration 配置(可选项)
+     * */
+    fun init(applicationContext: Context, configuration: DownloadConfiguration?) {
+        // 防止重复初始化
+        if (init) {
+            return
+        }
+
+        init = true
+        configuration?.let {
+            downloadConfiguration = configuration
+        }
+        // 初始化数据库
+        DbManager.downloadDatabase = Room.databaseBuilder(
+            applicationContext,
+            DownloadDataBase::class.java, "DownloadDataBase"
+        ).build()
+    }
 
     /**
      * 用户调用该方法，进行任务的下载
