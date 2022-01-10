2df89dcce8e0e80eb6fe64d04897e68acd8c413c
usagi <1023192250@qq.com>
2022-01-10 21:42:17 +0800

    feat(video): add new components for HorizontalVideoScreen

---
+++ video/src/main/java/com/topview/purejoy/video/ui/horizontal/HorizontalSpeedList.kt
@@ -0,0 +1,65 @@
+package com.topview.purejoy.video.ui.horizontal
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.topview.purejoy.video.ui.state.HorizontalVideoScreenState
+import com.topview.purejoy.video.ui.state.PlaySpeed
+import com.topview.purejoy.video.ui.theme.HalfAlphaBlack
+import com.topview.purejoy.video.ui.theme.Pink
+
+/**
+ * 倍速选择列表
+ */
+@Composable
+internal fun HorizontalSpeedList(
+    modifier: Modifier = Modifier,
+    state: HorizontalVideoScreenState,
+    onItemClick: (PlaySpeed) -> Unit,
+) {
+
+    Surface(
+        modifier = modifier,
+        color = HalfAlphaBlack
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.padding(horizontal = 15.dp)
+        ) {
+            LazyColumn {
+                val itemModifier = Modifier
+                    .padding(vertical = 15.dp, horizontal = 30.dp)
+                items(PlaySpeed.values()) {
+                    if (it == state.playbackSpeed) {
+                        Text(
+                            text = "${it}X",
+                            color = Pink,
+                            fontSize = 17.sp,
+                            modifier = itemModifier
+                        )
+                    } else {
+                        Text(
+                            text = "${it}X",
+                            color = Color.White,
+                            fontSize = 17.sp,
+                            modifier = itemModifier
+                                .clickable {
+                                    onItemClick(it)
+                                }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
