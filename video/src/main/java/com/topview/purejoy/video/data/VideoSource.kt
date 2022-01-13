c5d6581e3712b361a432ce06510ead9243f4bc64
usagi <1023192250@qq.com>
2022-01-13 22:52:10 +0800

    fix(video): fix passing params to paging3, fix the behavior of VideoSource

--- video/src/main/java/com/topview/purejoy/video/data/VideoSource.kt
+++ video/src/main/java/com/topview/purejoy/video/data/VideoSource.kt
@@ -3,14 +3,13 @@ package com.topview.purejoy.video.data
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.topview.purejoy.common.entity.Video
+import com.topview.purejoy.common.net.await
 import com.topview.purejoy.video.data.api.VideoService
 import com.topview.purejoy.video.data.bean.toVideos
-import retrofit2.await
 
 internal class VideoSource(
     private val videoService: VideoService,
-    private val initialList: List<Video>,
-    private val maxPage: Int = Int.MAX_VALUE
+    private val initialList: List<Video>
 ): PagingSource<Int, Video>() {
 
     /**
@@ -24,26 +23,32 @@ internal class VideoSource(
     }
 
     override fun getRefreshKey(state: PagingState<Int, Video>): Int? {
-        return null
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Video> {
         return runCatching {
             val page = params.key ?: 1
             val prevPage = if (page == 1) null else page - 1
-            val nextPage = if (page < maxPage) page + 1 else null
             if (page == 1) {
-                LoadResult.Page(initialList, prevPage, nextPage)
+                LoadResult.Page(initialList, prevPage, page + 1)
             } else {
                 val response = videoService.getRelevantVideo(heuristicVideoId ?: "").await()
+                    ?: error("Cannot get json object")
                 if (response.code != 200) {
-                    error("error http code: ${response.code}")
+                    error("Response code is ${response.code}! message: " + response.message)
                 }
                 val result = response.toVideos()
-                if (result.isNotEmpty()) {
+                val nextPage: Int? = if (result.isEmpty()) {
+                    null
+                } else {
                     // 更新推荐值
                     val index: Int = (result.size * Math.random()).toInt()
                     heuristicVideoId = result[index].id
+                    page + 1
                 }
                 LoadResult.Page(result, prevPage, nextPage)
             }
