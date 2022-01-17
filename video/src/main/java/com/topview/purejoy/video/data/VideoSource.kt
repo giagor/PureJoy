b2f697f53543ad3a348667a6f194f5b6d056529d
usagi <1023192250@qq.com>
2022-01-17 18:56:14 +0800

    fix(video): fix the error in coroutine use

--- video/src/main/java/com/topview/purejoy/video/data/VideoSource.kt
+++ video/src/main/java/com/topview/purejoy/video/data/VideoSource.kt
@@ -3,9 +3,11 @@ package com.topview.purejoy.video.data
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.topview.purejoy.common.entity.Video
-import com.topview.purejoy.common.net.await
+import com.topview.purejoy.common.net.awaitSync
 import com.topview.purejoy.video.data.api.VideoService
 import com.topview.purejoy.video.data.bean.toVideos
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 internal class VideoSource(
     private val videoService: VideoService,
@@ -30,30 +32,33 @@ internal class VideoSource(
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Video> {
-        return runCatching {
-            val page = params.key ?: 1
-            val prevPage = if (page == 1) null else page - 1
-            if (page == 1) {
-                LoadResult.Page(initialList, prevPage, page + 1)
-            } else {
-                val response = videoService.getRelevantVideo(heuristicVideoId ?: "").await()
-                    ?: error("Cannot get json object")
-                if (response.code != 200) {
-                    error("Response code is ${response.code}! message: " + response.message)
-                }
-                val result = response.toVideos()
-                val nextPage: Int? = if (result.isEmpty()) {
-                    null
+        return withContext(Dispatchers.IO) {
+            runCatching {
+                val page = params.key ?: 1
+                val prevPage = if (page == 1) null else page - 1
+                if (page == 1) {
+                    LoadResult.Page(initialList, prevPage, page + 1)
                 } else {
-                    // 更新推荐值
-                    val index: Int = (result.size * Math.random()).toInt()
-                    heuristicVideoId = result[index].id
-                    page + 1
+                    val response =
+                        videoService.getRelevantVideo(heuristicVideoId ?: "").awaitSync()
+                            ?: error("Cannot get json object")
+                    if (response.code != 200) {
+                        error("Response code is ${response.code}! message: " + response.message)
+                    }
+                    val result = response.toVideos()
+                    val nextPage: Int? = if (result.isEmpty()) {
+                        null
+                    } else {
+                        // 更新推荐值
+                        val index: Int = (result.size * Math.random()).toInt()
+                        heuristicVideoId = result[index].id
+                        page + 1
+                    }
+                    LoadResult.Page(result, prevPage, nextPage)
                 }
-                LoadResult.Page(result, prevPage, nextPage)
+            }.getOrElse {
+                LoadResult.Error(it)
             }
-        }.getOrElse {
-            LoadResult.Error(it)
         }
     }
 }
