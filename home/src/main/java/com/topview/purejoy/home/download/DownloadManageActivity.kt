576230b3c2850e6e3e5847d1d6bdf5458c4f83b7
18822835767 <1165407654@qq.com>
2022-01-19 21:43:13 +0800

    feat(home): Add DownloadManageActivity

---
+++ home/src/main/java/com/topview/purejoy/home/download/DownloadManageActivity.kt
@@ -0,0 +1,12 @@
+package com.topview.purejoy.home.download
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.topview.purejoy.home.R
+
+class DownloadManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_download_manage)
+    }
+}
