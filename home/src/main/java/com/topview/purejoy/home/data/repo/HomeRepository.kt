a9e65389b0cbd70d81ae3e48f63b677c20e17432
18822835767 <1165407654@qq.com>
2022-02-12 23:31:04 +0800

    perf(home): Use awaitAsync when net request

--- home/src/main/java/com/topview/purejoy/home/data/repo/HomeRepository.kt
+++ home/src/main/java/com/topview/purejoy/home/data/repo/HomeRepository.kt
@@ -4,49 +4,32 @@ import com.topview.purejoy.home.data.bean.SongDetailJson
 import com.topview.purejoy.home.data.source.HomeLocalStore
 import com.topview.purejoy.home.data.source.HomeRemoteStore
 import com.topview.purejoy.home.entity.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 internal object HomeRepository {
     private val homeRemoteStore: HomeRemoteStore = HomeRemoteStore()
     private val homeLocalStore: HomeLocalStore = HomeLocalStore()
 
-    suspend fun getBanners(): List<HomeDiscoverBannerItem>? = withContext(Dispatchers.IO) {
-        homeRemoteStore.getBanners()
-    }
+    suspend fun getBanners(): List<HomeDiscoverBannerItem>? = homeRemoteStore.getBanners()
 
     suspend fun getDailyRecommendPlayList(limit: Int): List<PlayList>? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.getDailyRecommendPlayList(limit)
-        }
+        homeRemoteStore.getDailyRecommendPlayList(limit)
 
     suspend fun getRecommendNewSong(limit: Int): List<Song>? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.getRecommendNewSong(limit)
-        }
+        homeRemoteStore.getRecommendNewSong(limit)
 
     suspend fun getSearchSongByFirst(keyword: String, limit: Int): SongPagerWrapper? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.getSearchSongByFirst(keyword, limit)
-        }
+        homeRemoteStore.getSearchSongByFirst(keyword, limit)
+
 
     suspend fun loadMoreSongs(keyword: String, offset: Int, limit: Int): List<Song>? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.loadMoreSongs(keyword, offset, limit)
-        }
+        homeRemoteStore.loadMoreSongs(keyword, offset, limit)
 
     suspend fun getSearchPlayListByFirst(keyword: String, limit: Int): PlayListPagerWrapper? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.getSearchPlayListByFirst(keyword, limit)
-        }
+        homeRemoteStore.getSearchPlayListByFirst(keyword, limit)
 
     suspend fun loadMorePlayLists(keyword: String, offset: Int, limit: Int): List<PlayList>? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.loadMorePlayLists(keyword, offset, limit)
-        }
+        homeRemoteStore.loadMorePlayLists(keyword, offset, limit)
 
     suspend fun requestSongUrl(id: Long): SongDetailJson? =
-        withContext(Dispatchers.IO) {
-            homeRemoteStore.requestSongUrl(id)
-        }
+        homeRemoteStore.requestSongUrl(id)
 }
